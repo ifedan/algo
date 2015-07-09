8767e9b6857b8e46d3195a3ca298db660fc619b3
ifedan <igor.fedan@gmail.com>
2015-07-09 17:17:10 -0400

    Update InsertionSort.java

--- algos/sort/InsertionSort.java
+++ algos/sort/InsertionSort.java
@@ -8,10 +8,10 @@ public class InsertionSort {
 
     public static void sort(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            for (int j = i; j > 0 & a[j] > a[j-1]; j--) {    //TODO: Reverse order
-                a[j] = a[j]^a[j-1];
-                a[j-1] = a[j]^a[j-1];
-                a[j] = a[j]^a[j-1];
+            for (int j = i; j > 0 && a[j] > a[j-1]; j--) {
+                int c = a[j];
+                a[j] = a[j-1];
+                a[j-1] = c;
             }
         }
     }
