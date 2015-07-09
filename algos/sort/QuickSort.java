d4037a441bfab8c8f6dfa9d2b4e97378148a07e6
ifedan <igor.fedan@gmail.com>
2015-07-09 17:37:57 -0400

    Update QuickSort.java

--- algos/sort/QuickSort.java
+++ algos/sort/QuickSort.java
@@ -6,41 +6,31 @@ package algos.sort;
  */
 public class QuickSort {
 
-    private static void sort(int a[]) {
+    public static void sort(int a[]) {
         sort(a, 0, a.length - 1);
     }
 
-    private static void sort(int a[], int lo, int hi) {
-        if (hi <= lo) return;  //TODO: !!!!!!!!
-        int j = partition(a, lo, hi);
-        sort(a, lo, j-1);
-        sort(a, j+1, hi);
-    }
-
-    private static int partition(int a[], int lo, int hi) {
-        int base = a[lo];
-
-        int i = lo;
-        int j = hi + 1;
-
-        while (true) {
-            while (a[++i]<=base) {
-                if (i == hi) break;
-            }
-            while (a[--j]>base) {
-                if (j==lo) break;
-            }
-            if (i>=j) break;
-
-            a[i] = a[i]^a[j];
-            a[j] = a[i]^a[j];
-            a[i] = a[i]^a[j];
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi > lo) {
+            int mid = (hi - lo) / 2 + lo;
+            int i = lo;
+            int j = hi;
+            do {
+                while (a[i] < a[mid]) i++;
+                while (a[j] > a[mid]) j--;
+                if (i<=j) {
+                    swap(a, i++, j--);
+                }
+            } while (i<=j);
+            sort(a, lo, j);
+            sort(a, i, hi);
         }
-
-        a[lo] = a[j];
-        a[j] = base;
-
-        return j;
+    }
+    
+    private static void swap(int[] a, int i, int j) {
+        int c = a[i];
+        a[i] = a[j];
+        a[j] = c;
     }
 
 
