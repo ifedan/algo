d8c7ed34be32b85e7197ca3864a5eb37cb56b145
ifedan <igor.fedan@gmail.com>
2015-07-28 17:24:28 -0400

    Update MergeSort.java

--- algos/sort/MergeSort.java
+++ algos/sort/MergeSort.java
@@ -6,33 +6,31 @@ package algos.sort;
  */
 public class MergeSort {
 
-    private static void merge(int[] a, int[] aux, int lo, int hi, int mid) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k]=a[k];
-        }
-        int i = 0;
-        int j = mid + 1;
-
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) {
-                a[k] = aux[j++];
-            } else if (j > hi) {
-                a[k] = aux[i++];
-            } else if (aux[i] > aux[j]) {
-                a[k] = a[j++];
-            } else if (aux[i] <= aux[j]) {
-                a[k] = a[i++];
-            }
-        }
+    public static void sort(int[] a) {
+        int[] aux = new int[a.length];
+        sort(a, aux, 0, a.length);
     }
 
-    public static void sort(int a[]) {
-        int aux[] = new int[a.length];
-        for (int hs = 1; hs < a.length; hs = (hs + hs)) {
-            for (int lo = 0; lo < a.length - hs; lo = lo + (hs + hs)) {
-                int mid = lo + hs - 1;
-                int hi = Math.min(lo + hs + hs - 1, a.length - 1);     //TODO: High index it's a min of high and length
-                merge(a, aux, lo, hi, mid);
+    private static void sort(int[] a, int[] aux, int lo, int hi) {
+        if (hi-lo > 1) {
+            int mid = (hi-lo)/2 + lo;
+
+            sort(a, aux, lo, mid);
+            sort(a, aux, mid, hi);
+
+            for (int i = lo; i < hi; i++) {
+                aux[i] = a[i];
+            }
+
+            int i = lo;
+            int j = mid;
+            int k = lo;
+            while (k < hi) {
+                if ((i < mid && j < hi && aux[i] < aux[j]) || j >= hi) {
+                    a[k++] = aux[i++];
+                } else {
+                    a[k++] = aux[j++];
+                }
             }
         }
     }
