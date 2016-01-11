09f9b3f42fe8b20059c589530bcd957cf4d96efc
ifedan <igor.fedan@gmail.com>
2016-01-11 13:26:40 -0500

    Update RadixSort.java

--- algos/sort/RadixSort.java
+++ algos/sort/RadixSort.java
@@ -8,38 +8,69 @@ import java.util.LinkedList;
  */
 public class RadixSort {
 
-
-    public static void sort(int a[]) {
-        LinkedList<Integer>[] buckets = new LinkedList[10];
+    public static void lcs(int a[], int maxLength, int optionCount) {
         int d = 1;
-
-        boolean flag = true;
-        while (flag) {
-
-            flag = false;
+        while (d <= maxLength) {
+            int[] count = new int[optionCount];
+            int[] copy = new int[a.length];
 
             for (int i = 0; i < a.length; i++) {
-                int hashIndex = (a[i]/d)%10;
-                if (hashIndex > 0) {   //TODO: CONTINUE UNTIL ALL NUMBERS IN 0-bucket
-                    flag = true;
-                }
-                if (buckets[hashIndex] == null) {
-                    buckets[hashIndex] = new LinkedList<Integer>();
-                }
-                buckets[hashIndex].add(a[i]);
+                int n = getValueAt(a[i], d);
+                count[n]++;
             }
 
-            int k = 0;
-            for (LinkedList<Integer> ll : buckets) {
-                if (ll == null) continue;
-                while (!ll.isEmpty()) {
-                    a[k++] = ll.removeFirst();
-                }
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i-1];
             }
 
-            d = d * 10;
+            for (int i = a.length - 1; i>=0; i--) {
+                int n = getValueAt(a[i], d);
+                copy[count[n]-1] = a[i];
+                count[n]--;
+            }
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = copy[i];
+            }
+            d++;
         }
+    }
+    
+    private static void mcs(String[] a, int maxLength, int optionCount) {
+        int d = maxLength;
+        while (d >= 0) {
+            int[] count = new int[optionCount];
+            String[] copy = new String[a.length];
 
+            for (int i = 0; i < a.length; i++) {
+                int n = getValueAt(a[i], d);
+                count[n]++;
+            }
+
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i-1];
+            }
+
+            for (int i = a.length - 1; i>=0; i--) {
+                int n = getValueAt(a[i], d);
+                copy[count[n]-1] = a[i];
+                count[n]--;
+            }
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = copy[i];
+            }
+            d--;
+        }
+    }
+    
+    private static int getValueAt(int a, int pos) {
+        int d = (int) Math.pow(10, pos);
+        return (a/d)%10;
+    }
+
+    private static int getValueAt(String a, int pos) {
+        return a.length() <= pos ? 0 : a.charAt(pos) - 'a';
     }
 
 }
