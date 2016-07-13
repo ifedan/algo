687ba832d6559907b0b2621b83a7d9e148bd7cd9
ifedan <igor.fedan@gmail.com>
2016-07-13 14:22:54 -0400

    Update QuickUF.java

--- algos/QuickUF.java
+++ algos/QuickUF.java
@@ -1,13 +1,10 @@
 package algos;
 
-/**
- * User: ifedan
- * Date: 4/21/15
- */
 public class QuickUF {
 
-    int[] tree;
-    int[] weights;
+    private int[] tree;
+    private int[] weights;
+    private int count;
 
     public QuickUF(int size) {
         tree = new int[size];
@@ -16,13 +13,22 @@ public class QuickUF {
             tree[i] = i;
             weights[i] = 1;
         }
+        count = size;
     }
 
     public boolean connected(int i, int j) {
-        return findRoot(i) == findRoot(j);
+        return (i == j) || (findRoot(i) == findRoot(j));
+    }
+    
+    public int count() {
+        return count;
     }
 
     public void union(int i, int j) {
+        if (i == j) {
+            return;
+        }
+        
         int rootI = findRoot(i);
         int rootJ = findRoot(j);
 
@@ -32,11 +38,14 @@ public class QuickUF {
 
         if (weights[rootI] > weights[rootJ]) {   //TODO: ADD SMALL TO LARGE
             tree[rootJ] = rootI;
-            weights[rootI] = weights[rootJ] + weights[rootI];
+            weights[rootI] += weights[rootJ];
+            compress(j, rootI);
         } else {
             tree[rootI] = rootJ;
-            weights[rootJ] = weights[rootJ] + weights[rootI];
+            weights[rootJ] += weights[rootI];
+            compress(i, rootJ);
         }
+        count--;
     }
 
     private int findRoot(int x) {
@@ -46,5 +55,12 @@ public class QuickUF {
         return x;
     }
 
+    private void compress(int i, int newParent) {
+        while (i != data[i]) {
+            int parent = data[i];
+            data[i] = newParent;
+            i = parent;
+        }
+    }
 
 }
