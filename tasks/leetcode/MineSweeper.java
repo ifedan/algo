44036f44563c8f55e27de2a89dd63be1dd14f4b9
ifedan <igor.fedan@gmail.com>
2017-04-03 15:42:18 -0400

    Update MineSweeper.java

--- tasks/leetcode/MineSweeper.java
+++ tasks/leetcode/MineSweeper.java
@@ -12,49 +12,51 @@ public class MineSweeper {
             board[click[0]][click[1]] = 'X';
             return board;
         }
+        int h = board.length;
+        int w = board[0].length;
 
-        Set<Long> visited = new HashSet<>();
-        Queue<Long> queue = new LinkedList<>();
-        long pos = toLong(click[0], click[1]);
-        queue.add(pos);
+        LinkedList<int[]> queue = new LinkedList<>();
+        queue.add(click);
         while (!queue.isEmpty()) {
-            pos = queue.poll();
-            visited.add(pos);
-            int x = (int) (pos>>32);
-            int y = (int) pos;
+            int[] pos = queue.poll();
+            int x = pos[0];
+            int y = pos[1];
+
             int count = 0;
+            int added = 0;
             for (int i=-1; i<=1; i++) {
-                for (int j=-1; j<=1; j++) {
-                    if (has(board, x+i, y+j,'M')) {
+                for (int j = -1; j<=1; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int x1 = x+i;
+                    int y1 = y+j;
+                    if (x1 < 0 || y1 < 0 || x1 >= h || y1 >= w) continue;
+                    if (board[x1][y1] == 'M') {
                         count++;
+                        continue;
+                    }
+                    if (count == 0 && board[x1][y1] == 'E') {
+                        queue.add(new int[]{x1, y1});
+                        added++;
+                    }
+                }
+            }
+            if (added > 0) {
+                Iterator<int[]> it = queue.descendingIterator();
+                for (int i = 0; i < added; i++) {
+                    int[] addedPos = it.next();
+                    if (count == 0) {
+                        board[addedPos[0]][addedPos[1]] = 'B';
+                    } else {
+                        it.remove();
                     }
                 }
             }
             if (count > 0) {
                 board[x][y] = Character.forDigit(count, 10);
             } else {
-                for (int i=-1; i<=1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (has(board, x+i, y+j,'E')) {
-                            board[x+i][y+j] = 'B';
-                            pos = toLong(x+i, y+j);
-                            if (!visited.contains(pos)) {
-                                queue.offer(pos);
-                            }
-                        }
-                    }
-                }
+                board[x][y] = 'B';
             }
-
         }
         return board;
     }
-
-    private boolean has(char[][] board, int x, int y, char expected) {
-        return x >= 0 && y >= 0 && x < board.length && y < board[x].length && board[x][y] == expected;
-    }
-
-    private long toLong(int x, int y) {
-        return ((long)x)<<32 | y;
-    }
-  }
+}
