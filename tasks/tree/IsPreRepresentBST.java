7594c5787af01017fbdffe1a4471d3c3ab336d48
ifedan <igor.fedan@gmail.com>
2018-04-24 12:29:36 -0400

    Update IsPreRepresentBST.java

--- tasks/tree/IsPreRepresentBST.java
+++ tasks/tree/IsPreRepresentBST.java
@@ -7,7 +7,7 @@ public class IsPreRepresentBST {
     public static boolean solve(int[] pre) {
         Stack<Integer> stack = new Stack<>();
 
-        int root = Integer.MAX_VALUE;
+        int root = Integer.MIN_VALUE;
 
         for (int i = 0; i < pre.length; i++) {
             if (pre[i] < root) {
