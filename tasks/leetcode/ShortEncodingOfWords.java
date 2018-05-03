513138894e93bc030c875ae3898aec4177e7adfd
ifedan <igor.fedan@gmail.com>
2018-05-03 14:17:18 -0400

    Create ShortEncodingOfWords.java

---
+++ tasks/leetcode/ShortEncodingOfWords.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ShortEncodingOfWords {
+    public static int minimumLengthEncoding(String[] words) {
+        if (words.length == 1) return words[0].length() + 1;
+        words = Arrays.stream(words).map(w -> new StringBuilder(w).reverse().toString()).sorted().toArray(String[]::new);
+        int total = 0;
+        for (int i = words.length-1; i >= 0;) {
+            total += words[i].length() + 1;
+            i--;
+            while (i >=0 && words[i+1].contains(words[i])) i--;
+        }
+        return total;
+    }
+}
