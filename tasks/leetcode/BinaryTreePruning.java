09befe5ea791dfb846c62d7645078b51beeb3c57
ifedan <igor.fedan@gmail.com>
2018-05-03 14:15:34 -0400

    Create BinaryTreePruning.java

---
+++ tasks/leetcode/BinaryTreePruning.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class BinaryTreePruning {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = pruneTree(root.left);
+        TreeNode right = pruneTree(root.right);
+        if (left == null && right == null && root.val == 0) return null;
+        TreeNode r = new TreeNode(root.val);
+        r.left = left;
+        r.right = right;
+        return r;
+    }
+}
